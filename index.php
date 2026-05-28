cde41bf5eeea5a578661d01932eb17d35b36052e
seanat0r <seanatorhd@gmail.com>
2026-05-28 17:34:32 +0200

    add: main content styles and refactored index.php

--- index.php
+++ index.php
@@ -15,18 +15,19 @@
                 <img src="assets/img/Kiba.png" alt="">
             </figure>
             <div class="text-content">
-                <p>Willkommen beim Cosplay-Atelier, deiner Cosplay Family im Aargau!<br><br>
-                    Hier wird Fiktion zur Realität. Der Verein fördert den Zusammenhalt und die Gemeinschaft unter
+                <p>Willkommen beim Cosplay-Atelier, deiner Cosplay Family im Aargau!</p>
+                <br> <p>Hier wird Fiktion zur Realität. Der Verein fördert den Zusammenhalt und die Gemeinschaft unter
                     Cosplayern und Interessierten. Es wird gemeinsam gebastelt, Conventions besucht, eigene Events und
                     Charity organisiert und durchgeführt, Videospiele gespielt, zusammen die Zeit verbracht… Der
                     Fantasie sind keine Grenzen gesetzt.</p>
-                <div class ="external-link">
-                    <p>Folgt uns auch auf anderen Plattformen um immer auf dem Laufenden zu bleiben!</p>
+
+                <div class="external-link">
+                    <p>Folgt uns auch auf anderen Plattformen, um immer auf dem Laufenden zu bleiben!</p>
                     <ul>
-                        <li><a href="https://www.facebook.com/CosplayAtelier.ch/" target="_blank">Facebook</a></li>
-                        <li><a href="https://www.instagram.com/cosplayatelier.ch/" target="_blank">Instagram</a></li>
-                        <li><a href="https://cosplay-atelier.ch/discord" target="_blank">Discord</a></li>
-                        <li><a href="https://www.youtube.com/channel/UCG9ZdVCMMVPZEbnB3jK1YDg/" target="_blank">Youtube</a></li>
+                        <li><a class="facebook" href="https://www.facebook.com/CosplayAtelier.ch/" target="_blank">Facebook</a></li>
+                        <li><a class="instagram" href="https://www.instagram.com/cosplayatelier.ch/" target="_blank">Instagram</a></li>
+                        <li><a class="discord" href="https://cosplay-atelier.ch/discord" target="_blank">Discord</a></li>
+                        <li><a class="youtube" href="https://www.youtube.com/channel/UCG9ZdVCMMVPZEbnB3jK1YDg/" target="_blank">YouTube</a></li>
                     </ul>
                 </div>
             </div>
